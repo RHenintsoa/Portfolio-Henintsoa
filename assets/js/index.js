cb8534a7313794690a6597cef076b2ddc5d638a2
RHenintsoa <henintsoarasolofohery@gmail.com>
2023-09-18 12:38:05 +0300

    insert button download CV

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+   //Déclaration des variables 
+var modal = document.getElementById("myModal");
+var openModalBtn = document.getElementById("openModalBtn");
+var downloadBtn = document.getElementById("downloadBtn");
+var cancelBtn = document.getElementById("cancelBtn");
+
+// boutton d'ouverture du modal
+openModalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+// bouton annuler
+cancelBtn.onclick = function(){
+    modal.style.display ="none";
+}
+// Confirmer le téléchragement
+downloadBtn.onclick = function() {
+  window.location.href = 'CV Henintsoa.pdf';
+  modal.style.display = "none";
+}
 
 });
